2925a0e9287baa50240b16d1d7838b77c291823f
AtsukiYokota <atsuki.yokota@gmail.com>
2018-10-18 19:57:42 +0900

    Modify youcompleteme install command to python3 from python2

--- scripts/install_vimycm.sh
+++ scripts/install_vimycm.sh
@@ -3,5 +3,5 @@
 scripts=$(cd "$(dirname $0)"; pwd)
 dotfiles=$(cd "$(dirname $0)"; cd ..; pwd)
 
-python2 $dotfiles/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
+python3 $dotfiles/.vim/bundle/YouCompleteMe/install.py --clang-completer --system-libclang
 # python2 $dotfiles/.vim/bundle/YouCompleteMe/install.py --all --system-libclang
